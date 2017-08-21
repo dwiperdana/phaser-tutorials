f37065be2caca4f223baf77973e239207e91c0eb
Aditia Dwiperdana <deepest_peak@yahoo.com>
2017-08-21 20:54:03 +0700

    add basic input

--- 01/game.js
+++ 01/game.js
@@ -1,55 +1,58 @@
 console.clear();
 
+var ball;
 var game = new Phaser.Game({
   // antialias:   true,
   // enableDebug: true,
-   height:      320,
+  height: 600,
   // renderer:    Phaser.AUTO,
   // resolution:  1,
   // scaleMode:   Phaser.ScaleManager.NO_SCALE,
   // transparent: false,
-   width:       480,
-   parent: "gameDiv",
+  width: 800,
+  parent: "game",
   state: {
-
-    init: function() {
-
-    },
-
-    preload: function() {
-      // See <https://github.com/samme/phaser-examples-assets>
-      this.load.baseURL = 'https://cdn.jsdelivr.net/gh/samme/phaser-examples-assets@v1.0.0/';
-      this.load.crossOrigin = 'anonymous';
-      this.load.image('dude', 'sprites/phaser-dude.png');
-      this.load.image('ball','https://raw.githubusercontent.com/dwiperdana/phaser-tutorials/master/01/asset/ballGrey.png')
-    },
-
-    create: function() {
-      var sprite = this.add.sprite(this.world.centerX, this.world.centerY, 'ball');
-      // this.physics.enable(sprite);
-    },
-
-    update: function() {
-
-    },
-
-    render: function() {
-      // var debug = this.game.debug;
-      this.renderLegend(10, 310);
-    },
-
-    shutdown: function() {
-
-    },
-
-    renderLegend: function(x, y) {
-      var debug = this.game.debug;
-      debug.text([
-        'Phaser', Phaser.VERSION,
-        ['AUTO', 'CANVAS', 'WEBGL', 'HEADLESS', 'WEBGL_MULTI'][game.renderType],
-        // navigator.userAgent,
-      ].join(' '), x, y, 'white', debug.font);
-    }
-
+    init: init,
+    preload: preload,
+    create: create,
+    update: update,
+    render: render,
+    shutdown: shutdown
   }
 });
+
+function init() {
+}
+
+function preload() {
+  this.load.crossOrigin = 'anonymous';
+  this.load.image('ball', 'asset/ballGrey.png');
+}
+
+function create() {
+  ball = this.add.sprite(this.world.centerX, this.world.centerY, 'ball');
+}
+
+function update() {
+  if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
+    ball.x -= 1;
+  } else if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
+    ball.x += 1;
+  }
+}
+
+function render() {
+  renderLegend(10, 590);
+}
+
+function renderLegend(x, y) {
+  var debug = this.game.debug;
+  debug.text([
+    'Phaser', Phaser.VERSION, ['AUTO', 'CANVAS', 'WEBGL', 'HEADLESS', 'WEBGL_MULTI'][game.renderType],
+    // navigator.userAgent,
+  ].join(' '), x, y, 'white', debug.font);
+}
+
+function shutdown() {
+
+}
